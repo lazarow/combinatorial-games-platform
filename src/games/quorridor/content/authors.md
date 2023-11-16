d3c598227e3991faec263f9366c92b453901fc20
funborg <jakub.golos@onet.pl>
2023-11-16 16:10:34 +0100

    dodanie quoridor

---
+++ src/games/quorridor/content/authors.md
@@ -0,0 +1,1 @@
+Jakub Gołoś, Damian Kozak 
---
+++ src/games/quorridor/js/game.logic.js
@@ -0,0 +1,66 @@
+const boardWidth = 9;
+const boardHeight = 9;
+
+const logicOfGame = {
+    /**
+     * Generuje stan początkowy gry.
+     */
+    generateInitialState() { 
+        return {
+        player1: [4, 0],
+        player2: [4, 8],
+        removed: [],
+        };
+    },
+    /**
+     * Funkcja oceny, która ocenia z punktu widzenia wskazanego gracza.
+     */
+    evaluateState(state, player) {
+        return 1;
+    },
+    /**
+     * Funkcja generująca możliwe ruchy z wskazanego stanu dla gracza.
+     */
+    generateMoves(state, player) {
+        const offsets = [
+            [ 0,  1],
+            [ 0, -1],
+            [ 1,  0],
+            [-1,  0],
+        ];
+        const moves = [];
+        for (let i = 0; i < offsets.length; ++i) {
+            const x = state[player][0] + offsets[i][0];
+            const y = state[player][1] + offsets[i][1];
+            if (x >= 0 && x < boardWidth && y >= 0 && y < boardHeight) {
+                     moves.push([x, y]);
+
+            }
+        }
+        return moves;
+    },
+    /**
+     * Funkcja generuje stan po wykonaniu wskazanego ruchu.
+     */
+    generateStateAfterMove(previousState, player, move) {
+        const state = {
+            player1: [...previousState.player1],
+            player2: [...previousState.player2],
+            removed: [...previousState.removed],
+        };
+        //state.removed.push(state[player]);
+        state[player] = move;
+        return state;
+    },
+    /**
+     * Funkcja sprawdza czy stan jest terminalny, tzn. koniec gry.
+     */
+    isStateTerminal(state, player) {
+    },
+    /**
+     * Funkcja generująca unikalny klucz dla wskazanego stanu.
+     */
+    generateUniqueKey: undefined,
+};
+
+const players = [];
