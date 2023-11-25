8ff7402c07f74a181a113deb4784a5a5629a9620
Fr4gFronti3r <56560896+Fr4gFronti3r@users.noreply.github.com>
2023-11-25 11:30:33 +0100

    Added logic and visuals for Mankala
    Added the basic logic to play Mankala with the Turkish ruleset.
    
    Added visuals to represent a Mankala board with "stores"
    
    Added basic user interaction

--- src/games/mankala/js/game.logic.js
+++ src/games/mankala/js/game.logic.js
@@ -1,29 +1,88 @@
 /**
  * Nazwa katalogu gry, potrzebne do wczytywanie skryptów.
  */
-const gameId = "template";
+const gameId = "mankala";
 
 const logicOfGame = {
     /**
      * Generuje stan początkowy gry.
      */
-    generateInitialState() {},
+    generateInitialState() {
+        return {
+            player1: { pits: [4, 4, 4, 4, 4, 4], store: 0 },
+            player2: { pits: [4, 4, 4, 4, 4, 4], store: 0 },
+        };
+    },
     /**
      * Funkcja oceny, która ocenia z punktu widzenia wskazanego gracza.
      */
-    evaluateState(state, player) {},
+    evaluateState(state, player) {
+        return state[player].store;
+    },
     /**
      * Funkcja generująca możliwe ruchy z wskazanego stanu dla gracza.
      */
-    generateMoves(state, player) {},
+    generateMoves(state, player) {
+        const playerBoard = state[player].pits;
+        const moves = [];
+        for (let i = 0; i < 6; i++) {
+            if (playerBoard[i] > 0) {
+                moves.push(i);
+            }
+        }
+        return moves;
+    },
     /**
      * Funkcja generuje stan po wykonaniu wskazanego ruchu.
      */
-    generateStateAfterMove(previousState, player, move) {},
+    generateStateAfterMove(previousState, player, move) {
+        const state = JSON.parse(JSON.stringify(previousState));
+        let currentPlayer = player;
+        let currentBoard = state[currentPlayer].pits;
+        let currentStore = state[currentPlayer].store;
+        const opponent = currentPlayer === "player1" ? "player2" : "player1";
+        const opponentBoard = state[opponent].pits;
+        let stones = currentBoard[move];
+        currentBoard[move] = 0;
+        let i = move + 1;
+        let lastPit = -1;
+        while (stones > 0) {
+            if (i % 7 !== 6 || stones === 1) { // Only increment the store when the last stone is being placed
+                if (i % 7 === 6) {
+                    currentStore++;
+                } else {
+                    currentBoard[i % 7]++;
+                }
+                stones--;
+                lastPit = i % 7;
+            }
+            i++;
+        }
+        state[currentPlayer].pits = currentBoard;
+        state[currentPlayer].store = currentStore;
+        if (lastPit !== 6) { // If the last stone didn't land in the store, switch players
+            currentPlayer = currentPlayer === "player1" ? "player2" : "player1";
+        }
+        state[opponent].pits = opponentBoard;
+        return state;
+    },
     /**
      * Funkcja sprawdza czy stan jest terminalny, tzn. koniec gry.
      */
-    isStateTerminal(state, player) {},
+    isStateTerminal(state, player) {
+        const opponent = player === "player1" ? "player2" : "player1";
+        const allPitsEmpty = (pits) => pits.every(pit => pit === 0);
+        if (allPitsEmpty(state[player].pits)) {
+            state[player].store += state[opponent].pits.reduce((a, b) => a + b);
+            state[opponent].pits = state[opponent].pits.map(() => 0);
+            return true;
+        }
+        if (state.turns >= 300) {
+            console.log("Too many turns");
+            return true;
+        }
+        return false;
+    },
     /**
      * Funkcja generująca unikalny klucz dla wskazanego stanu.
      */
--- src/games/mankala/js/game.visualization.js
+++ src/games/mankala/js/game.visualization.js
@@ -3,22 +3,62 @@ const visualizationOfGame = {
      * Funkcja rysuje planszę do gry w wskazanym kontenerze (720 pikseli szerokości).
      * Po zakończeniu rysowania należy wykonać funkcję `cb`.
      */
-    drawState(state, player, move, container, cb) {},
+    drawState(state, player, move, container, cb) {
+        const player1Board = state["player1"].pits;
+        const player2Board = state["player2"].pits;
+        const player1Store = state["player1"].store;
+        const player2Store = state["player2"].store;
+
+        let board = '<div class="mancala-board">';
+        board += `<div class="player2-store">${player2Store}</div>`;
+        board += '<div class="pits-row">';
+        for (let i = 0; i < 6; i++) {
+            board += `<div class="pit player2-pit">${player2Board[5 - i]}</div>`;
+        }
+        board += '</div>';
+        board += '<div class="pits-row">';
+        for (let i = 0; i < 6; i++) {
+            board += `<div class="pit player1-pit">${player1Board[i]}</div>`;
+        }
+        board += '</div>';
+        board += `<div class="player1-store">${player1Store}</div>`;
+        board += "</div>";
+
+        container.innerHTML = board;
+        cb();
+    },
     /**
      * Funkcja włącza tryb interaktywny dla gracza. Po wykonaniu ruchu należy wykonać funkcję `cb` a na jej
      * wejście podać wybrany ruch.
-     */
-    handleHumanTurn(state, player, cb) {},
+    */
+
+    handleHumanTurn(state, player, cb) {
+        const playerPits = document.querySelectorAll(`.${player}-pit`);
+
+        playerPits.forEach((pit, i) => {
+            pit.addEventListener("click", () => {
+                if (state[player].pits[i] !== 0) {
+                    cb(i);
+                }
+            });
+        });
+    },
     /**
      * Funkcja zwraca nazwę gracza zgodną z zasadami.
      */
-    getTruePlayerName(player) {},
+    getTruePlayerName(player) {
+        return player === "player1" ? "Player 1" : "Player 2";
+    },
     /**
      * Funkcja zwraca czytelny dla człowieka opis ruchu.
      */
-    getReadableMoveDescription(state, player, move) {},
+    getReadableMoveDescription(state, player, move) {
+        return `Player ${player === "player1" ? "1" : "2"} moves ${move + 1}`;
+    },
     /**
      * Funkcja zwraca czytelny dla człowieka opis wygranego gracza.
      */
-    getReadableWinnerName(state, player) {},
+    getReadableWinnerName(state, player) {
+        return `Player ${player === "player1" ? "1" : "2"}!`;
+    },
 };
