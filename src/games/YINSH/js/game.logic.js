d1d735e948c1e03145ccf53cb997f15de1782fc5
Rafał Rusin <rafal.rusin01@gmail.com>
2023-11-28 20:53:15 +0100

    Drawing map

--- src/games/YINSH/js/game.logic.js
+++ src/games/YINSH/js/game.logic.js
@@ -2,15 +2,51 @@ const logicOfGame = {
     /**
      * Generuje stan początkowy gry.
      */
-    generateInitialState() {},
+    generateInitialState() {
+        return {
+            placement_done: false,
+            player1: {
+                rings: [],
+                pawns: [],
+                points: 0
+            },
+            player2: {
+                rings: [],
+                pawns: [],
+                points: 0
+            },
+            positions: [
+                [4, 0], [6, 0], [3, 1], [5, 1], [7, 1],
+                [2, 2], [4, 2], [6, 2], [8, 2], [1, 3],
+                [3, 3], [5, 3], [7, 3], [9, 3], [2, 4],
+                [4, 4], [6, 4], [8, 4], [1, 5], [3, 5],
+                [5, 5], [7, 5], [9, 5], [0, 6], [2, 6],
+                [4, 6], [6, 6], [8, 6], [10, 6], [1, 7],
+                [3, 7], [5, 7], [7, 7], [9, 7], [0, 8],
+                [2, 8], [4, 8], [6, 8], [8, 8], [10, 8],
+                [1, 9], [3, 9], [5, 9], [7, 9], [9, 9],
+                [0, 10], [2, 10], [4, 10], [6, 10], [8, 10],
+                [10, 10], [1, 11], [3, 11], [5, 11], [7, 11],
+                [9, 11], [0, 12], [2, 12], [4, 12], [6, 12],
+                [8, 12], [10, 12], [1, 13], [3, 13], [5, 13],
+                [7, 13], [9, 13], [2, 14], [4, 14], [6, 14],
+                [8, 14], [1, 15], [3, 15], [5, 15], [7, 15],
+                [9, 15], [2, 16], [4, 16], [6, 16], [8, 16],
+                [3, 17], [5, 17], [7, 17], [4, 18], [6, 18]
+            ]
+        }
+    },
     /**
      * Funkcja oceny, która ocenia z punktu widzenia wskazanego gracza.
      */
-    evaluateState(state, player) {},
+    evaluateState(state, player) {
+    },
     /**
      * Funkcja generująca możliwe ruchy z wskazanego stanu dla gracza.
      */
-    generateMoves(state, player) {},
+    generateMoves(state, player) {
+
+    },
     /**
      * Funkcja generuje stan po wykonaniu wskazanego ruchu.
      */
@@ -18,7 +54,9 @@ const logicOfGame = {
     /**
      * Funkcja sprawdza czy stan jest terminalny, tzn. koniec gry.
      */
-    isStateTerminal(state, player) {},
+    isStateTerminal(state, player) {
+        return state[player].points === 3;
+    },
     /**
      * Funkcja generująca unikalny klucz dla wskazanego stanu.
      */
--- src/games/YINSH/js/game.visualization.js
+++ src/games/YINSH/js/game.visualization.js
@@ -3,12 +3,35 @@ const visualizationOfGame = {
      * Funkcja rysuje planszę do gry w wskazanym kontenerze (720 pikseli szerokości).
      * Po zakończeniu rysowania należy wykonać funkcję `cb`.
      */
-    drawState(state, player, move, container, cb) {},
+    drawState(state, player, move, container, cb) {
+        let board = "<table>"
+        for (let y=0; y<19; y++)
+        {
+            board += "<tr>"
+            for (let x=0; x<11; x++)
+            {
+                if (state.positions.some(pos => pos[0] === x && pos[1] === y))
+                {
+                    board += "<td class='dot'></td>"
+                }
+                else
+                {
+                    board += "<td></td>"
+                }
+            }
+            board += "</tr>"
+        }
+        board += "</table>"
+        container.innerHTML = board;
+        cb();
+    },
     /**
      * Funkcja włącza tryb interaktywny dla gracza. Po wykonaniu ruchu należy wykonać funkcję `cb` a na jej
      * wejście podać wybrany ruch.
      */
-    handleHumanTurn(state, player, cb) {},
+    handleHumanTurn(state, player, cb) {
+
+    },
     /**
      * Funkcja zwraca nazwę gracza zgodną z zasadami.
      */
