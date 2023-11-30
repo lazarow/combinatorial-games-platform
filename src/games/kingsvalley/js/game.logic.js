56a11398a69ba8bc2edddcf3bba5d159e610bfc4
f1shu <szulzyckifilip@gmail.com>
2023-11-30 16:10:01 +0100

    Add King's Valley

--- src/games/kingsvalley/js/game.logic.js
+++ src/games/kingsvalley/js/game.logic.js
@@ -1,31 +1,98 @@
 const boardWidth = 5;
 const boardHeight = 5;
 
+const gameId = "kingsvalley";
+
 const logicOfGame = {
-    /**
-     * Generuje stan początkowy gry.
-     */
-    generateInitialState() {},
-    /**
-     * Funkcja oceny, która ocenia z punktu widzenia wskazanego gracza.
-     */
-    evaluateState(state, player) {},
-    /**
-     * Funkcja generująca możliwe ruchy z wskazanego stanu dla gracza.
-     */
-    generateMoves(state, player) {},
-    /**
-     * Funkcja generuje stan po wykonaniu wskazanego ruchu.
-     */
-    generateStateAfterMove(previousState, player, move) {},
-    /**
-     * Funkcja sprawdza czy stan jest terminalny, tzn. koniec gry.
-     */
-    isStateTerminal(state, player) {},
-    /**
-     * Funkcja generująca unikalny klucz dla wskazanego stanu.
-     */
+    generateInitialState() {
+        return {
+            player1: [0, 0, 1, 0, 2, 0, 3, 0, 4, 0],
+            player2: [0, 4, 1, 4, 2, 4, 3, 4, 4, 4]
+        };
+    },
+    evaluateState() {
+        // TODO
+    },
+    generateMoves(state, player) {
+        const offsets = [
+            [0, 1], [0, -1], [1, 0], [-1, 0],
+            [1, 1], [1, -1], [-1, 1], [-1, -1]
+        ];
+        const pawns = [
+            state[player].slice(0, 2),
+            state[player].slice(2, 4),
+            state[player].slice(4, 6),
+            state[player].slice(6, 8),
+            state[player].slice(8),
+        ]
+        const opponent = player === "player1" ? "player2" : "player1";
+        const opponentPawns = [
+            state[opponent].slice(0, 2),
+            state[opponent].slice(2, 4),
+            state[opponent].slice(4, 6),
+            state[opponent].slice(6, 8),
+            state[opponent].slice(8)
+        ];
+        let moves = [];
+        const kingPawn = 2;
+        for (let [pawn, position] of pawns.entries()) {
+            for (let [offsetX, offsetY] of offsets) {
+                let [x, y] = position;
+                let [prevX, prevY] = [x, y];
+                while (true) {
+                    x += offsetX;
+                    y += offsetY;
+                    if (
+                        x < 0 || y < 0 || x >= boardWidth || y >= boardHeight
+                        || pawns.some(([_x, _y]) => x === _x && y === _y)
+                        || opponentPawns.some(([_x, _y]) => x === _x && y === _y)
+                    ) {
+                        break;
+                    }
+                    prevX = x;
+                    prevY = y;
+                }
+                if (prevX === position[0] && prevY === position[1]) continue;
+                if (pawn !== kingPawn && prevX === 2 && prevY === 2) continue;
+                moves.push([pawn, prevX, prevY]);
+            }
+        }
+        return moves;
+    },
+    generateStateAfterMove(previousState, player, move) {
+        const state = {
+            player1: [...previousState.player1],
+            player2: [...previousState.player2]
+        };
+        if (move[0] === 0) {
+            state[player][0] = move[1]
+            state[player][1] = move[2]
+        }
+        if (move[0] === 1) {
+            state[player][2] = move[1]
+            state[player][3] = move[2]
+        }
+        if (move[0] === 2) {
+            state[player][4] = move[1]
+            state[player][5] = move[2]
+        }
+        if (move[0] === 3) {
+            state[player][6] = move[1]
+            state[player][7] = move[2]
+        }
+        if (move[0] === 4) {
+            state[player][8] = move[1]
+            state[player][9] = move[2]
+        }
+        return state;
+    },
+    isStateTerminal(state) {
+        return (state.player1[4] === 2 && state.player1[5] === 2)
+            || (state.player2[4] === 2 && state.player2[5] === 2);
+    },
     generateUniqueKey: undefined,
 };
 
-const players = [];
+const players = [
+    { type: PlayerTypes.ALPHABETA, label: "AlphaBeta (łatwy)", maxDepth: 3 }
+];
--- src/games/kingsvalley/js/game.visualization.js
+++ src/games/kingsvalley/js/game.visualization.js
@@ -1,24 +1,99 @@
+var currentPiece
 const visualizationOfGame = {
-    /**
-     * Funkcja rysuje planszę do gry w wskazanym kontenerze (720 pikseli szerokości).
-     * Po zakończeniu rysowania należy wykonać funkcję `cb`.
-     */
-    drawState(state, player, move, container, cb) {},
-    /**
-     * Funkcja włącza tryb interaktywny dla gracza. Po wykonaniu ruchu należy wykonać funkcję `cb` a na jej
-     * wejście podać wybrany ruch.
-     */
-    handleHumanTurn(state, player, cb) {},
-    /**
-     * Funkcja zwraca nazwę gracza zgodną z zasadami.
-     */
-    getTruePlayerName(player) {},
-    /**
-     * Funkcja zwraca czytelny dla człowieka opis ruchu.
-     */
-    getReadableMoveDescription(state, player, move) {},
-    /**
-     * Funkcja zwraca czytelny dla człowieka opis wygranego gracza.
-     */
-    getReadableWinnerName(state, player) {},
+    drawState(state, player, move, container, cb) {
+        let board = '<table class="board">';
+        board += "<tr><td></td>";
+        for (let x = 0; x < boardWidth; ++x) {
+            board += "<td><label>" + String.fromCharCode(97 + x) + "</label></td>";
+        }
+        board += "</tr>";
+        for (let y = boardHeight - 1; y >= 0; --y) {
+            board += "<tr><td><label>" + (y + 1) + "</label></td>";
+            for (let x = 0; x < boardWidth; ++x) {
+                board +=
+                    '<td class="square' + (x === Math.floor(boardWidth / 2) && y === Math.floor(boardHeight / 2) ? ' center' : '') + '"><div class="square-placeholder" data-x="' +
+                    x +
+                    '" data-y="' +
+                    y +
+                    '">';
+                if (state.player1[0] === x && state.player1[1] === y) board += '<div class="white-pawn" id="player1_0"></div>';
+                if (state.player1[2] === x && state.player1[3] === y) board += '<div class="white-pawn" id="player1_1"></div>';
+                if (state.player1[4] === x && state.player1[5] === y) board += '<div class="white-king" id="player1_2"></div>';
+                if (state.player1[6] === x && state.player1[7] === y) board += '<div class="white-pawn" id="player1_3"></div>';
+                if (state.player1[8] === x && state.player1[9] === y) board += '<div class="white-pawn" id="player1_4"></div>';
+                if (state.player2[0] === x && state.player2[1] === y) board += '<div class="black-pawn" id="player2_0"></div>';
+                if (state.player2[2] === x && state.player2[3] === y) board += '<div class="black-pawn" id="player2_1"></div>';
+                if (state.player2[4] === x && state.player2[5] === y) board += '<div class="black-king" id="player2_2"></div>';
+                if (state.player2[6] === x && state.player2[7] === y) board += '<div class="black-pawn" id="player2_3"></div>';
+                if (state.player2[8] === x && state.player2[9] === y) board += '<div class="black-pawn" id="player2_4"></div>';
+
+                board += "</div></td>";
+            }
+            board += "<td><label>" + (y + 1) + "</label></td>";
+            board += "</tr>";
+        }
+        board += "<tr><td></td>";
+        for (let x = 0; x < boardWidth; ++x) {
+            board += "<td><label>" + String.fromCharCode(97 + x) + "</label></td>";
+        }
+        board += "</tr>";
+        board += "</table>";
+        container.innerHTML = board;
+        cb();
+    },
+
+    handleHumanTurn(state, player, cb) {
+        const moves = logicOfGame.generateMoves(state, player);
+        const pawns = [
+            $("#" + player + "_0"),
+            $("#" + player + "_1"),
+            $("#" + player + "_2"),
+            $("#" + player + "_3"),
+            $("#" + player + "_4")
+        ];
+        for (let [pawn, pawnEl] of pawns.entries()) {
+            pawnEl.draggable({
+                scope: "fields",
+                revert: "invalid",
+                revertDuration: 0,
+                refreshPositions: true,
+                start() {
+                    let fields = moves.filter(move => move[0] === pawn).map(([, x, y]) => {
+                        return $(".square-placeholder[data-x=" + x + "][data-y=" + y + "]")
+                    })
+                    fields.forEach(field => field.addClass("highlighted"));
+                    fields.forEach(field => {
+                        field.droppable({
+                            accept: pawnEl,
+                            scope: "fields",
+                            drop(event, ui) {
+                                ui.draggable.appendTo(this);
+                                ui.draggable.css("top", 0);
+                                ui.draggable.css("left", 0);
+                                fields.forEach(field => field.droppable("destroy"));
+                                pawns.forEach(pawnEl => pawnEl.draggable("destroy"));
+                                cb([pawn, parseInt(ui.draggable.parent().attr("data-x")), parseInt(ui.draggable.parent().attr("data-y"))]);
+                            }
+                        });
+                    })
+                },
+                stop() {
+                    let fields = moves.filter(move => move[0] === pawn).map(([, x, y]) => {
+                        return $(".square-placeholder[data-x=" + x + "][data-y=" + y + "]")
+                    })
+                    fields.forEach(field => field.removeClass("highlighted"));
+                },
+            })
+        }
+    },
+    getTruePlayerName(player) {
+        if (player === "player1") return "Biały";
+        if (player === "player2") return "Czarny";
+    },
+    getReadableMoveDescription(state, player, move) {
+        return String.fromCharCode(97 + move[0]) + (move[1] + 1);
+    },
+    getReadableWinnerName(state, player) {
+        return player === "player1" ? "Czarny" : "Biały";
+    },
 };
