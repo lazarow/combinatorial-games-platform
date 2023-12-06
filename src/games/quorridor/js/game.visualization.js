2539ee4f4f673f5a4bf9d2f35fc6bd380444a87b
funborg <50322001+funborg@users.noreply.github.com>
2023-12-06 10:39:31 +0100

    add quoridor (#9)
    * dodanie quoridor
    
    * dodanie nie funkcjonujących płotków
    
    * stworzenie poruszania bez kolizji płotków
    
    * Dodanie autorów, opisu i drobnych zmian w css i game logic
    
    * dodanie interakcji płotków
    
    * dodanie podstawowej logiki płotka i uzupełnienie game state
    
    płotki jeszcze się nie pojawiają ale nie można kliknąć w to samo miejsce i ograniczenie płotków działa
    
    * Dodanie blokowania się gracza o płotki
    
     wersja z jednym, ręcznie postawionym płotkiem - do późniejszej modyfikacji
    
    * dodanie wizualnego działania płotków
    
    * minor fix
    
    * kolizja płotków dodana
    
    * Poprawka do blokowania się pionków o płotki
    
    blokowanie się pionka o płotek działa już na tablicy occupied, samo blokowanie jest zrobione w osobnej funkcji, dodana została też inna funkcja, która sprawdza, czy istnieje płotek pomiędzy dwoma pionkami w momencie kiedy stoją obok siebie
    
    * lekkie przemodelowanie checkFencesOnTheWay
    
    teraz funkcja sprawdza pozycje pomiędzy dwoma sąsiadującymi polami
    
    * nie działający przeskok fix
    
    źle if'y dałem
    
    * Dodanie liczników postawionych płotków
    
    * dodano sprawdzanie blokowania gracza
    
    jeszcze się przyda poczyścić kod
    
    * Wprowadzenie zmian wizualnych
    
    * naprawienie błedu w obliczaniu drogi i uproszczenie kodu
    
    * lekka zmiana opisu ruchu
    
    * Poprawka w zmianach wizualnych
    
    * poprawki nowakowskiego
    
    * Update game.visualization.js
    
    * Update package-lock.json
    
    ---------
    
    Co-authored-by: xTMx3 <damkoz824@gmail.com>

---
+++ src/games/quorridor/js/game.visualization.js
@@ -0,0 +1,260 @@
+const visualizationOfGame = {
+    /**
+     * Funkcja rysuje planszę do gry w wskazanym kontenerze (720 pikseli szerokości).
+     * Po zakończeniu rysowania należy wykonać funkcję `cb`.
+     */
+    drawState(state, player, move, container, cb) {
+        debug = 1
+        // Utworzenie planszy
+        let board = '<table class="board">';
+
+        // Utworzenie liczników o ilości możliwych do postawienia płotków
+        board += "<div class = 'fenceCounterA'>";
+        board += "<a class = 'textA'>Płotki<a class = 'space'></a>";
+        board += "<div class = 'pawnWhite'></div></a>";
+        board += "<a class = 'countTextA'>x" + state.player1fences + "</a>";
+
+        board += "<div class = 'fenceCounterB'>";
+        board += "<a class = 'textB'>Płotki<a class = 'space'></a>";
+        board += "<div class = 'pawnBlack'></div></a>";
+        board += "<a class = 'countTextB'>x" + state.player2fences + "</a></div></div>";
+
+        // Dodanie pustej komórki na górze planszy (aby wyrównać oznaczenia pól na planszy)
+        board += "<tr><td></td>";
+
+        /* NOTATKA O NUMEROWANIU KOLUMN
+           Nieparzyste numery kolumn są numerowane od początku alfabetu (począwszy od 'a' i idące wzwyż np. a b c d ...), 
+           natomiast parzyste od końca (począwszy od 'z' i idące wstecz np. z y x w v ...),
+           gracz powinien móc poruszać się po polach parzystych, natomiast pola o kolumnach i wierszach nieparzystych oznaczają pola, gdzie może zostać postawiony płotek i
+           gracz nie może się na takim polu znaleźć 
+        */
+
+        // Oznaczenia kolumn (górne)
+        /*
+        for (let x = 0; x < boardWidth-1; ++x) {
+            if (x % 2 === 0) {
+                board += '<td class="even"><label>' + String.fromCharCode(97 + x / 2) + '</label></td>';
+            } else {
+                board += '<td class="odd"></td>';
+            }
+        }
+        */
+        // Zamknięcie wiersza z oznaczeniami pól
+        board += "</tr>";
+
+        // Zmienna pomocnicza do oznaczania wierszy i kolumn płotków
+        let temp = true;
+
+        // Numerowanie wierszy z lewej strony
+        for (let y = boardHeight - 1; y >= 0; --y) {
+
+            // Oznaczenia tylko dla wierszy parzystych (kwadratów)
+            if (y % 2 === 0) {
+                board += '<tr class="even"><td>';
+                if ((((y / 2) + 1) % 2 != 0)) {
+                    board += '<label class = "labelEven">' + (y / 2 + 1) + '</label></td>';
+                } else {
+                    board += '<label class = "labelOdd">' + (y / 2 + 1) + '</label></td>';
+                }
+
+                // Wiersze z polami pionków na przemiennie z płotkami wertykalnymi
+                for (let x = 0; x < boardWidth - 1; ++x) {
+                    const isOccupied = state.occupied.some(([occupiedX, occupiedY]) => x === occupiedX && y === occupiedY);
+                    if (x % 2 == 0) {
+                        board += '<td class="even"> <div class= "square" data-x ="' + x + '" data-y="' + y + '">';
+                    } else {
+                        if (!(y == 0))
+                            board += '<td class="odd"><div class="fence vertical ' + (isOccupied ? "placed" : "") + '" data-x ="' + x + '" data-y="' + y + '">';
+                        else
+                            board += '<td class="odd"><div>'
+                    }
+                    //dodanie pionków
+                    if (state.player1[0] === x && state.player1[1] === y)
+                        board += '<div id="whitepawn">';
+
+                    if (state.player2[0] === x && state.player2[1] === y)
+                        board += '<div id="blackpawn">';
+
+                    board += "</div></td>";
+                }
+                // Wstawienie płotków horyzontalnych pomiędzy wierszami
+            } else if (!(y === boardHeight - 1)) {
+                board += '<tr class = "odd"<td></td>';
+                for (x = 0; x <= boardHeight - 1; x += 2) {
+                    const isOccupied = state.occupied.some(([occupiedX, occupiedY]) => x === occupiedX && y === occupiedY);
+                    if (x === 0) {
+                        if (temp) {
+                            board += '<td class="firstOdd"></td>';
+                            temp = false;
+                        } else {
+                            board += '<td class="firstEven"></td>';
+                            temp = true;
+                        }
+                    } else {
+                        board += '<td></td>';
+                    }
+                    board += '<td class="even"><div class="fence horizontal ' + (isOccupied ? "placed" : "") + '" data-x="' + x + '" data-y="' + y + '">';
+                }
+                board += "</tr>";
+            }
+        }
+
+        // Zerowanie zmiennej pomocnicznej, dodanie pustego wiersza i oznaczeń dla pól (kolumn) na dole planszy
+        board += '<tr><td></td>';
+
+        // Numerowanie kolumn z dołu
+        for (let x = 0; x < boardWidth; ++x) {
+            // Ustawienie oznaczeń tylko dla pól parzystych (kwadratów)
+            if (x % 2 === 0) {
+                board += '<td>';
+                if ((97 + x / 2) % 2 === 0) {
+                    board += '<label class = "labelEven">' + String.fromCharCode(97 + x / 2) + '</label></td>';
+                } else {
+                    board += '<label class = "labelOdd">' + String.fromCharCode(97 + x / 2) + '</label></td>';
+                }
+            } else {
+                if (x == boardWidth - 1) {
+                    board += '<td></td>';
+                } else {
+                    if (temp) {
+                        board += '<td class = "firstEven"></td>';
+                        temp = false;
+                    } else {
+                        board += '<td class = "firstOdd"></td>';
+                        temp = true;
+                    }
+                }
+            }
+        }
+
+        // Zamknięcie tabeli
+        board += '</tr>';
+        board += '</table>';
+        // Ustawienie HTML planszy w kontenerze
+        container.innerHTML = board;
+
+        // Wywołanie funkcji zwrotnej (callback)
+        cb();
+    },
+    /**
+     * Funkcja włącza tryb interaktywny dla gracza. Po wykonaniu ruchu należy wykonać funkcję `cb` a na jej
+     * wejście podać wybrany ruch.
+     */
+    handleHumanTurn(state, player, cb) {
+        // Pobranie referencji do pionka gracza
+        const pawn = $("#" + (player === "player1" ? "white" : "black") + "pawn");
+        const pawnX = state[player][0];
+        const pawnY = state[player][1];
+        // Wygenerowanie możliwych ruchów
+        const moves = logicOfGame.generateMoves(state, player);
+
+
+        let fieldsList = "";
+        let i = 0;
+        // Utworzenie listy pól, na które można przesunąć pionek
+        //zakłada że ruchy pionków są pierwsze
+        while ((moves[i][0] % 2 === 0 && moves[i][1] % 2 === 0)) {
+            const field = $(".square[data-x=" + moves[i][0] + "][data-y=" + moves[i][1] + "]");
+            if (field.length > 0 && field.is(":empty")) {
+                fieldsList +=
+                    (fieldsList.length > 0 ? ", " : "") +
+                    ".square[data-x=" + moves[i][0] +
+                    "][data-y=" + moves[i][1] + "]";
+            }
+            i++;
+            if (moves[i] === undefined)
+                break;
+        }
+        // Utworzenie listy płotków, na które można postawić
+        let fencelist = "";
+        for (i; i < moves.length; i++) {
+            let type = ".fence".concat(moves[i][1] % 2 === 0 ? ".vertical" : ".horizontal");
+            /*
+            let type
+            if(moves[i][1]%2===0){
+                 type = ".fence"+ ".vertical";
+            }else{
+                 type = ".fence"+ ".horizontal";
+            }*/
+            const fence = $(type + "[data-x=" + moves[i][0] + "][data-y=" + moves[i][1] + "]")
+            if (fence.length > 0)
+                fencelist +=
+                    (fencelist.length > 0 ? ", " : "") +
+                    type + "[data-x=" + moves[i][0] +
+                    "][data-y=" + moves[i][1] + "]";
+        }
+
+        // Pobranie referencji do pól, na które można przesunąć pionek
+        const fields = $(fieldsList);
+        const blockades = $(fencelist);
+        // Ustawienie pionka jako przesuwalnego
+        pawn.draggable({
+            scope: "fields",
+            revert: "invalid",
+            refreshPositions: true,
+            start() {
+                fields.addClass("highlighted");
+            },
+            stop() {
+                fields.removeClass("highlighted");
+                fields.removeClass("highlighted2");
+            },
+        });
+
+
+
+        // Ustawienie pól jako miejsca, na które można przesunąć pionek
+        fields.droppable({
+            accept: "#whitepawn, #blackpawn",
+            scope: "fields",
+            drop(event, ui) {
+                ui.draggable.appendTo(this);
+                cb([parseInt(ui.draggable.parent().attr("data-x")), parseInt(ui.draggable.parent().attr("data-y"))]);
+            },
+            over() {
+                $(this).addClass("highlighted2");
+            },
+            out() {
+                $(this).removeClass("highlighted2");
+            },
+        });
+        //dodanie interakcji do płotków
+        blockades.on({
+            mouseenter() {
+                $(this).addClass("hover");
+            },
+            mouseleave() {
+                $(this).removeClass("hover");
+            },
+            click() {
+                $(this).addClass("placed");
+                $(this).removeClass("hover");
+                cb([parseInt($(this).attr("data-x")), parseInt($(this).attr("data-y"))]);
+
+            },
+        });
+    },
+    /**
+     * Funkcja zwraca nazwę gracza zgodną z zasadami.
+     */
+    getTruePlayerName(player) {
+        if (player === "player1") return "Biały";
+        if (player === "player2") return "Czarny";
+    },
+    /**
+     * Funkcja zwraca czytelny dla człowieka opis ruchu.
+     */
+    getReadableMoveDescription(state, player, move) {
+
+        if (move[0] % 2 === 0 && move[1] % 2 === 0)
+            return (player === "player1" ? "B " : "C ") + "R: " + String.fromCharCode(97 + move[0] / 2) + ((move[1] / 2) + 1);
+        else
+            return (player === "player1" ? "B " : "C ") + "P: " + (move[1] % 2 === 0 ? "W" : "H") + String.fromCharCode(97 + Math.floor(move[0] / 2)) + (Math.ceil((move[1] / 2)))
+    },
+    /**
+     * Funkcja zwraca czytelny dla człowieka opis wygranego gracza.
+     */
+    getReadableWinnerName(state, player) {
+        return player === "player2" ? "Biały" : "Czarny";
+    },
+};
