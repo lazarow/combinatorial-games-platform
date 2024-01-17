80196ad6b9840b42734d5c33609bba2d6a937cb8
Arkadiusz Nowakowski <nowakowski.arkadiusz@gmail.com>
2024-01-17 23:59:35 +0100

    wip

--- src/games/joust/js/game.logic.js
+++ src/games/joust/js/game.logic.js
@@ -6,8 +6,8 @@ const gameId = "joust";
 const logicOfGame = {
     generateInitialState() {
         return {
-            player1: [Math.floor(Math.random() * boardWidth), 0],
-            player2: [Math.floor(Math.random() * boardWidth), boardHeight - 1],
+            player1: [3, 0],
+            player2: [4, boardHeight - 1],
             removed: [],
         };
     },
@@ -45,11 +45,11 @@ const logicOfGame = {
             [-1, -2],
         ];
         const moves = [];
+        const invalidPositions = [state.player1, state.player2, ...state.removed];
         for (let i = 0; i < offsets.length; ++i) {
             const x = state[player][0] + offsets[i][0];
             const y = state[player][1] + offsets[i][1];
             if (x >= 0 && x < boardWidth && y >= 0 && y < boardHeight) {
-                const invalidPositions = [state.player1, state.player2, ...state.removed];
                 if (invalidPositions.some(([invalidX, invalidY]) => x === invalidX && y === invalidY) === false) {
                     moves.push([x, y]);
                 }
@@ -102,6 +102,18 @@ const logicOfGame = {
             }
         }
         return bestNode;
+    },
+    getFitnessFunctionNofGames() {
+        return 100;
+    },
+    getBoardAsNeuralNetworkInput(state) {
+        let inputBoard = [...Array(boardHeight * boardWidth)].fill(0);
+        inputBoard[state.player1[1] * boardWidth + state.player1[0]] = 1;
+        inputBoard[state.player2[1] * boardWidth + state.player2[0]] = 2;
+        for (let [x, y] of state.removed) {
+            inputBoard[y * boardWidth + x] = -1;
+        }
+        return inputBoard;
     },
 };
 
